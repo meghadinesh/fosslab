322b8e5a303c4ea2fb113b80e9542b40b2fd411b
meghadinesh <meghadinesh3@gmail.com>
2018-03-22 06:32:15 +0000

    Delete shell1.sh

--- shell1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Login name : $LOGNAME"
-echo "User name :  $USER"
-echo "Current shell : $SHELL"
-echo "Home directory : $HOME" 
-echo "Operating System : $OSTYPE"
-echo "Current path : $PATH"
-echo "Current working directory : `pwd`"
-echo "No.of users currently logged in:`who | wc -l`"
